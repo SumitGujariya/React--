fa12b6aad56848138bf02ef76e15d271b99a84a5
SumitGujariya <gujariyasumit96@gmail.com>
2025-10-06 03:59:50 +0530

    Update both folders

---
+++ 01basicsreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>React On Youtube</h1>
+  );
+}
+
+export default App;
---
+++ 01vitebasics/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+   <h1>React On Youtube | sumit gujariya</h1>
+  )
+}
+
+export default App
