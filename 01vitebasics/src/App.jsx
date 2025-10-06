a4207554e69fd2369a3fe3911081de60c8b81826
SumitGujariya <gujariyasumit96@gmail.com>
2025-10-07 00:07:06 +0530

    add code component

--- 01vitebasics/src/App.jsx
+++ 01vitebasics/src/App.jsx
@@ -1,9 +1,13 @@
+import Code from "./code";
 
 function App() {
-
   return (
-   <h1>React On Youtube | sumit gujariya</h1>
-  )
+    <div>
+      <Code />
+      <h1>code is not ready</h1>
+      <h2>code is not working</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
