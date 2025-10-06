1c319924d8e3c996bc917e25d4888bfd30760f61
SumitGujariya <gujariyasumit96@gmail.com>
2025-10-07 00:27:25 +0530

    Update the App.jsx

--- 01vitebasics/src/App.jsx
+++ 01vitebasics/src/App.jsx
@@ -2,12 +2,16 @@ import Code from "./code";
 
 function App() {
   return (
-    <div>
-      <Code />
+    // <div> we called fragment in react with the help you write many elements in this.
+    // you also write like this - <> </> It's still valid in react.
+    <>  
+    <Code />
       <h1>code is not ready</h1>
       <h2>code is not working</h2>
-    </div>
+    </>
   );
 }
 
 export default App;
+
+{/* */}
