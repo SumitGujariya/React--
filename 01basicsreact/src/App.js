666266beb98488aaba97480cfb9ced188ce71886
SumitGujariya <gujariyasumit96@gmail.com>
2025-10-07 01:19:08 +0530

    update 01basicsreact folder

--- 01basicsreact/src/App.js
+++ 01basicsreact/src/App.js
@@ -2,6 +2,8 @@ import Code from "./Code";
 
 function App() {
   return (
+    // <div> we called fragment in react with the help you write many elements in this.
+    // you also write like this - <> </> It's still valid in react.
     <>
       <Code />
       <h1>React On Youtube</h1>
--- 01basicsreact/src/Code.js
+++ 01basicsreact/src/Code.js
@@ -1,3 +1,8 @@
+ {/* Try to write all the files name with capital letters.
+    
+Always remember you give it to the any function() name make sure it will start with the capital letter */}
+
+
 function Code(){
     return (
         <h1>code is ready</h1>
