25970fd10bbfefc78f9afab4370789418ddf856b
SumitGujariya <gujariyasumit96@gmail.com>
2025-10-10 23:48:32 +0530

    update the cuurent update

--- 1counterpr/src/main.jsx
+++ 1counterpr/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+//React automatically updates the UI
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
